3541393139606bfa40ec021f43ba446faef01f4b
TatyanaPolet <tatyana_polet@mail.ru>
2024-06-18 12:46:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+immutable_var = 6,12, 3.9, True, 'Air'
+print (immutable_var)# This is a sample Python script.
+# immutable_var [3] = False
+mutable_list = [6,12, 3.9, True, 'Air']
+print (mutable_list)
+mutable_list [2] = 6.8
+print (mutable_list)
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
